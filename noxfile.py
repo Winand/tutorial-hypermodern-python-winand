f008d24defb258b96bb46fca7763b168ff3f4d2f
Makarov Andrey <winandfx@gmail.com>
2023-01-21 12:51:43 +0300

    Nox: install with constraints

--- noxfile.py
+++ noxfile.py
@@ -7,11 +7,32 @@ nox.options.sessions = "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    """
+    Install packages taking into account version constraints in Poetry
+    """
+    # NamedTemporaryFile Permission denied https://stackoverflow.com/a/54768241
+    requirements = tempfile.NamedTemporaryFile(delete=False)
+    try:
+        session.run(
+            "poetry", "export",
+            "--with=dev", "--format=constraints.txt", "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+    finally:
+            requirements.close()
+            os.unlink(requirements.name)
+
+
 @nox.session(python=["3.9", "3.8"])
 def lint(session: nox.Session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-bandit", "flake8-black",
-                    "flake8-bugbear", "flake8-import-order")
+    install_with_constraints(session,
+        "flake8", "flake8-bandit", "flake8-black", "flake8-bugbear",
+        "flake8-import-order"
+    )
     session.run("flake8", *args)
 
 
@@ -25,7 +46,7 @@ def tests(session: nox.Session):
 @nox.session(python="3.9")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -40,7 +61,7 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
     finally:
         requirements.close()
