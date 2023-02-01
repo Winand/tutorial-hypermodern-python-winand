fcd4bcefeb5486ab103c09ebad061b144064b760
Makarov Andrey <winandfx@gmail.com>
2023-02-02 00:48:19 +0300

    tests nnotations

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,14 @@ modules at the same directory level can use them without explicit import.
 https://docs.pytest.org/en/latest/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 """
 
+from unittest.mock import Mock
+
 import pytest
+from pytest_mock import MockerFixture
 
 
 @pytest.fixture
-def mock_requests_get(mocker):
+def mock_requests_get(mocker: MockerFixture) -> Mock:
     mock = mocker.patch("requests.get")
     mock.return_value.__enter__.return_value.json.return_value = {
         "title": "Lorem Ipsum",
@@ -17,5 +20,5 @@ def mock_requests_get(mocker):
     return mock
 
 
-def pytest_configure(config):
+def pytest_configure(config: pytest.Config) -> None:
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
