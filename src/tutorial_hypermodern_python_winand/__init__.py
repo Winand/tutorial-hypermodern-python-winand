aae61575092549bc1a593352f8f7ba05665a02c8
Makarov Andrey <winandfx@gmail.com>
2023-02-10 22:43:30 +0300

    Version is specified in pyproject.toml

--- src/tutorial_hypermodern_python_winand/__init__.py
+++ src/tutorial_hypermodern_python_winand/__init__.py
@@ -1,3 +1,10 @@
 """The hypermodern Python project."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version  # type: ignore
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    # tried to get version of the package but it's not installed in environment
+    __version__ = "unknown"
